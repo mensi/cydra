b166d01535899d98d99c17223a83b605d31082be
Manuel Stocker <mensi@mensi.ch>
2013-03-29 16:45:53 +0100

    Add GPL header

--- plugins/activedirectory/cydraplugins/activedirectory/__init__.py
+++ plugins/activedirectory/cydraplugins/activedirectory/__init__.py
@@ -1,4 +1,21 @@
 # -*- coding: utf-8 -*-
+#
+# Copyright 2012 Manuel Stocker <mensi@mensi.ch>
+#
+# This file is part of Cydra.
+#
+# Cydra is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Cydra is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Cydra.  If not, see http://www.gnu.org/licenses
 import os.path
 import re
 
